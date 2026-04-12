4f5e49996223495c2aa2b2d3876cd6e1c5011e85
ncaq <ncaq@ncaq.net>
2026-04-12 21:22:19 +0900

    Merge pull request #503 from ncaq/clean-root-warn
    Explicitly set user in supervisord config

--- nixos-modules/microvm/virtiofsd/default.nix
+++ nixos-modules/microvm/virtiofsd/default.nix
@@ -16,7 +16,10 @@ in
     virtiofsd-run =
       let
         supervisordConfig = {
-          supervisord.nodaemon = true;
+          supervisord = {
+            nodaemon = true;
+            user = "root";
+          };
 
           "eventlistener:notify" = {
             command = pkgs.writers.writePython3 "supervisord-event-handler" { } (
